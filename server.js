2fc6834cdcceace34e2fdb120ace9d6335c3d409
Liv Huang <livhuang95@gmail.com>
2020-10-28 20:49:01 +0800

    建置Server

--- server.js
+++ server.js
@@ -4,5 +4,11 @@ const app = require("./app");
 const DB =
   "mongodb+srv://Liv:F62rDr5aAVSHpryP@cluster0.a7fw9.mongodb.net/members?retryWrites=true&w=majority";
 
-mongoose.connect(DB).then(() => console.log("連接成功：會員"));
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("連接成功：會員"));
 
+const server = app.listen(8000, "127.0.0.1", () => {
+  console.log(`from port: 8000`);
+});
