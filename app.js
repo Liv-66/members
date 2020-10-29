19ddbc9fb4abf12dbc4ff423806e431345d1194c
Liv Huang <livhuang95@gmail.com>
2020-10-29 10:12:09 +0800

    userRouter

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const userRouter = require("./routes/userRouter");
 
 const app = express();
+app.set("view engine", "hbs");
+
+app.use("/users", userRouter);
 
 module.exports = app;
