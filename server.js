e13c7cee3e21c1fdb2de6cdd0c56a821828f5ddf
Liv Huang <livhuang95@gmail.com>
2020-11-03 12:52:22 +0800

    準備部署

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-const DB =
-  "mongodb+srv://Liv:F62rDr5aAVSHpryP@cluster0.a7fw9.mongodb.net/members?retryWrites=true&w=majority";
+const DB = process.env.DATABASE;
 
-mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("連接成功：會員"));
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("連接成功：會員"));
 
-const server = app.listen(8000, "127.0.0.1", () => {
-  console.log(`from port: 8000`);
-});
+const server = app.listen(process.env.PORT || 3000, "127.0.0.1");
